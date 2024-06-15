aea0e1cefc3e14efb6cfcb4c5bbb8ecd0f8138d6
yahia-momtaz2000 <senior.steps.info@gmail.com>
2024-06-15 11:36:20 +0300

    Adding part1 to pyqt gui

---
+++ gui/login_module_ui.py
@@ -0,0 +1,49 @@
+# import QtGui module from PyQt5 package
+from PyQt5 import QtGui, uic
+
+# from QtWidgets found in package PyQt5 import classes QApplication - QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton, QMessageBox, QComboBox, \
+    QRadioButton, QCheckBox, QTextEdit
+import sys
+
+
+
+class LoginScreen(QMainWindow):  # inheritance FirstWindow class inherit from QMainWindow class
+    def __init__(self, master_module_reference):
+        super().__init__()
+        self.master_module_reference = master_module_reference
+
+        # Load ui File
+        uic.loadUi('login_module_ui.ui', self)
+
+        # Define Widgets
+        self.line_edit_username = self.findChild(QLineEdit, 'line_edit_username')
+        self.line_edit_password = self.findChild(QLineEdit, 'line_edit_password')
+        self.button_login = self.findChild(QPushButton, 'button_login')
+
+        # Define Operation
+        self.button_login.clicked.connect(self.func_check_login)
+
+        # Show window
+        self.show()
+
+    def func_check_login(self): # static        username = ahmed        password = ahmed123
+        # task : make dynamic check for user from a db table
+        user_name = self.line_edit_username.text()
+        password = self.line_edit_password.text()
+        print(user_name, password)
+        if user_name =='ahmed' and password == 'ahmed123':
+            print('Valid login - Welcome')
+            self.line_edit_username.setText('')
+            self.line_edit_password.setText('')
+            # hide login sub window , re-enable menu
+            self.master_module_reference.mdi_area.closeAllSubWindows()
+            self.master_module_reference.func_change_menu_visibility(True)
+        else:
+            print('Invalid user name or password - please try again')
+
+
+# main program
+# app_object = QApplication(sys.argv) # sys.argv : list of parameters used when open window from terminal [ not often ]
+# widgets_window_object = LoginScreen()
+# sys.exit(app_object.exec())  # exit screen only when user take action
---
+++ gui/master_module_ui.py
@@ -0,0 +1,62 @@
+# import QtGui module from PyQt5 package
+from PyQt5 import QtGui, uic
+
+# from QtWidgets found in package PyQt5 import classes QApplication - QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton, QMessageBox, QComboBox, \
+    QRadioButton, QCheckBox, QTextEdit, QMenu, QAction, QMdiArea
+import sys
+
+from gui import login_module_ui
+
+
+class MasterScreen(QMainWindow):  # inheritance FirstWindow class inherit from QMainWindow class
+    def __init__(self):
+        super().__init__()
+
+        # Load ui File
+        uic.loadUi('master_module_ui.ui', self)
+
+        # Define Widgets
+        # 1- define Menus
+        self.menu_user_actions = self.findChild(QMenu, 'menu_user_actions')
+        self.menu_products_setup = self.findChild(QMenu, 'menu_products_setup')
+
+        # 2- Define Menu items
+        self.action_logout = self.findChild(QAction, 'action_logout')
+        self.action_exit_application = self.findChild(QAction, 'action_exit_application')
+        self.action_software = self.findChild(QAction, 'action_software')
+        self.action_hardware = self.findChild(QAction, 'action_hardware')
+        self.action_manual = self.findChild(QAction, 'action_manual')
+
+        # 3- Define Mdi Area widget
+        self.mdi_area = self.findChild(QMdiArea, 'mdi_area')
+
+
+        # Define Operations ??
+
+        # Define initializers
+        self.showMaximized()  # maximize the screen to full screen
+        # maximize mdi area to full screen
+        scr = QApplication.desktop()
+        available_geometry = scr.availableGeometry()
+        self.mdi_area.setGeometry(available_geometry)
+
+        self.func_open_login_screen() # initially open the login screen
+
+        # Show window
+        self.show()
+
+
+    def func_open_login_screen(self):
+        self.login_screen = login_module_ui.LoginScreen(self)  # object from LoginScreen
+        self.mdi_area.addSubWindow(self.login_screen)
+        self.login_screen.show()
+
+    def func_change_menu_visibility(self, is_valid):
+        self.menu_user_actions.setEnabled(is_valid)
+        self.menu_products_setup.setEnabled(is_valid)
+
+# main program
+app_object = QApplication(sys.argv) # sys.argv : list of parameters used when open window from terminal [ not often ]
+widgets_window_object = MasterScreen()
+sys.exit(app_object.exec())  # exit screen only when user take action
